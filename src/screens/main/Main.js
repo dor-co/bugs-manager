2653422bcc9c18fe4b59e7158a18753cbcd94455
dor-co <dor713cohen@gmail.com>
2021-08-18 13:06:58 +0300

    init project

---
+++ src/screens/main/Main.js
@@ -0,0 +1,14 @@
+import './Style.css';
+
+function Main(){
+    return(
+        <div className='mainContainerStyle'>
+            <h1 className='titleStyle'>Bugs Manager</h1>
+            <div className='bodyStyle'>
+
+            </div>
+        </div>
+    );
+}
+
+export default Main;
