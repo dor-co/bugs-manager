5c77ab949f690b70d28fc58daf72961798ddb42a
dor-co <dor713cohen@gmail.com>
2021-08-22 15:37:14 +0300

    real time database update

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,11 +1,13 @@
 import './Style.css';
-import React from 'react';
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
 import { useSelector, useDispatch } from "react-redux";
 import { closeModal } from "../../redux/Actions";
 
@@ -14,14 +16,115 @@ function Modal() {
     const modalRed = useSelector((state) => state.ModalReducer);
     const dispatch = useDispatch();
 
-    console.log(modalRed)
+    const [titleErrorValid, setTitleErrorValid] = useState(false);
+    const [titleInput, setTitleInput] = useState("");
+    const [descriptionErrorValid, setDescriptionErrorValid] = useState(false);
+    const [descriptionInput, setDescriptionInput] = useState("");
+    const [statusErrorValid, setStatusErrorValid] = useState(false);
+    const [statusInput, setStatusInput] = useState("");
+
+    const titleBlur = (e) => {
+        setTitleInput(e.target.value);
+        if (e.target.value.trim() !== "") {
+            setTitleErrorValid(false);
+        } else {
+            setTitleErrorValid(true);
+        }
+    };
+
+    const titleChange = (e) => {
+        if (e.target.value.trim() !== "") {
+            setTitleErrorValid(false);
+        } else {
+            setTitleErrorValid(true);
+        }
+    };
+
+    const descriptionBlur = (e) => {
+        setDescriptionInput(e.target.value);
+        if (e.target.value.trim() !== "") {
+            setDescriptionErrorValid(false);
+        } else {
+            setDescriptionErrorValid(true);
+        }
+    };
+
+    const descriptionChange = (e) => {
+        if (e.target.value.trim() !== "") {
+            setDescriptionErrorValid(false);
+        } else {
+            setDescriptionErrorValid(true);
+        }
+    };
+
+    const statusBlur = (e) => {
+        if (e.target.value === undefined) {
+            e.target.value = "";
+        }
+        setStatusInput(e.target.value);
+        if (e.target.value.trim() !== "") {
+            setStatusErrorValid(false);
+        } else {
+            setStatusErrorValid(true);
+        }
+    };
+
+    const statusChange = (e) => {
+        setStatusInput(e.target.value);
+        if (e.target.value.trim() !== "") {
+            setStatusErrorValid(false);
+        } else {
+            setStatusErrorValid(true);
+        }
+    };
+
+    const addClick = () => {
+        if (titleInput.trim() === "") {
+            setTitleErrorValid(true);
+        } if (descriptionInput.trim() === "") {
+            setDescriptionErrorValid(true);
+        } if (statusInput.trim() === "") {
+            setStatusErrorValid(true);
+        } else if (titleInput.trim() !== "" && descriptionInput.trim() !== "" && statusInput.trim() !== "") {
+            //add to firebase
+            setTitleInput('');
+            setDescriptionInput('');
+            setStatusInput('')
+            return dispatch(closeModal());
+        }
+    };
 
     const handleClose = () => {
+        setTitleErrorValid(false);
+        setTitleInput('');
+        setDescriptionErrorValid(false);
+        setDescriptionInput('');
+        setStatusErrorValid(false);
+        setStatusInput('');
         return dispatch(closeModal());
-    }
+    };
+
+    const statusValues = [
+        {
+            value: 'todo',
+            label: 'todo',
+        },
+        {
+            value: 'in progress',
+            label: 'in progress',
+        },
+        {
+            value: 'ready for QA',
+            label: 'ready for QA',
+        },
+        {
+            value: 'done',
+            label: 'done',
+        },
+    ];
 
     return (
-            <div>
+        <div>
             <Dialog
                 open={modalRed.boolOpen}
                 onClose={handleClose}
@@ -31,7 +134,7 @@ function Modal() {
                 <DialogTitle id="alert-dialog-title">
                     {
                         modalRed.title === 'addTitle' ? 'Add New Bug' :
-                        modalRed.title === 'bugDetailsTitle' ? 'Bug Details' : ''
+                            modalRed.title === 'bugDetailsTitle' ? 'Bug Details' : ''
                     }
                 </DialogTitle>
                 <DialogContent>
@@ -44,16 +147,57 @@ function Modal() {
                                     <h3>Bug Description: {modalRed.firestoreRow.description}</h3>
                                     <h3>Bug Critical: {modalRed.firestoreRow.critical}</h3>
                                 </>
+                            ) : modalRed.body === 'addBody' ? (
+                                <>
+                                    <div>
+                                        <TextField
+                                            className='inputFieldStyle'
+                                            onBlur={titleBlur}
+                                            onChange={titleChange}
+                                            error={titleErrorValid ? true : false}
+                                            label="Bug Title"
+                                            helperText={titleErrorValid ? 'bug title is required' : ''}
+                                        />
+                                    </div>
+                                    <div>
+                                        <TextField
+                                            className='inputFieldStyle'
+                                            onBlur={descriptionBlur}
+                                            onChange={descriptionChange}
+                                            error={descriptionErrorValid ? true : false}
+                                            label="Bug Description"
+                                            helperText={descriptionErrorValid ? 'bug description is required' : ''}
+                                        />
+                                    </div>
+                                    <div>
+                                        <TextField
+                                            className='inputFieldStyle'
+                                            onBlur={statusBlur}
+                                            onChange={statusChange}
+                                            value={statusInput}
+                                            error={statusErrorValid ? true : false}
+                                            select
+                                            label="Bug Status"
+                                            helperText={statusErrorValid ? 'bug status is required' : ''}
+                                        >
+                                            {statusValues.map((option) => (
+                                                <MenuItem key={option.value} value={option.value}>
+                                                    {option.label}
+                                                </MenuItem>
+                                            ))}
+                                        </TextField>
+                                    </div>
+                                </>
                             ) : ''
-                            
-                            }
+
+                        }
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose} color="primary">
                         cancel
                     </Button>
-                    <Button onClick={handleClose} color="primary" autoFocus>
+                    <Button onClick={modalRed.body === 'addBody' ? addClick : handleClose} color="primary" autoFocus>
                         ok
                     </Button>
                 </DialogActions>
--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -1,13 +1,13 @@
 import './Style.css';
 import FadeIn from 'react-fade-in';
 import Button from "@material-ui/core/Button";
-import { useFirestore } from "reactfire";
 import "firebase/firestore";
 import React, { useState, useEffect } from 'react';
 import Bug from '../../components/bug/Bug';
 import { useSelector, useDispatch } from "react-redux";
 import { openModal, closeModal } from '../../redux/Actions';
 import Modal from '../../components/modal/Modal';
+import { useFirestore, useFirestoreDocData } from "reactfire";
 
 function Main() {
 
@@ -15,7 +15,7 @@ function Main() {
     const dispatch = useDispatch();
 
     const [firestoreData, setFirestoreData] = useState([]);
-
+    const [dataArray, setDataArray] = useState([]);
     const db = useFirestore();
 
     const useItems = (itemType, callback, items) => {
@@ -38,12 +38,85 @@ function Main() {
 
     useItems("Bugs", setFirestoreData, firestoreData);
 
+    useEffect(() => {
+        getFromDB();
+    }, []);
+
     const addBug = () => {
-        dispatch(openModal('addTitle'));
+        dispatch(openModal('addTitle', 'addBody'));
     }
+
+    const postToDB = async() => {
+        const response = await fetch('https://bugsmanager-9d88c-default-rtdb.firebaseio.com/bugs.json', {
+            method: 'POST',
+            body: JSON.stringify({firstname: 'dor', lastname: 'cohen'}),
+            headers: {
+                'Content-Type': 'application/json' 
+            }
+        });
+        const data = await response.json();
+        console.log('post data:', data);
+        
+        getFromDB();
+    };
+
+    const getFromDB = async() => {
+        const response = await fetch('https://bugsmanager-9d88c-default-rtdb.firebaseio.com/bugs.json');
+        const data = await response.json();
+
+        const loadedData = [];
+
+        for(const key in data){
+            loadedData.push({
+                id: key,
+                firstname: data[key].firstname,
+                lastname: data[key].lastname
+            });
+        }
+
+        setDataArray(loadedData);
+        console.log('loaded data:', loadedData);
+    };
+
+    const deleteFromDB = async(id) => {
+        const response = await fetch(`https://bugsmanager-9d88c-default-rtdb.firebaseio.com/bugs/${id}.json`, {
+            method: 'DELETE'
+          });
+
+        const data = await response.json();
+        console.log('data after delete:', data);
+        getFromDB();
+    };
+
+    const putToDB = async() => {
+        const response = await fetch('https://bugsmanager-9d88c-default-rtdb.firebaseio.com/bugs/-MhdWrT9_jXMsIgscKZ4.json', {
+            method: 'PUT',
+            body: JSON.stringify({firstname: 'dor111111', lastname: 'cohe'}),
+            headers: {
+                'Content-Type': 'application/json' 
+            }
+        });
+        const data = await response.json();
+        console.log('post data:', data);
+    };
 
     return (
         <div className='mainContainerStyle'>
+            <Button onClick={postToDB}>Add Data</Button>
+            <Button onClick={getFromDB}>Fetch Data</Button>
+            <Button onClick={deleteFromDB}>Delete Data</Button>
+            <Button onClick={putToDB}>Put Data</Button>
+
+            {console.log('$$',dataArray)}
+
+            {dataArray.map(item => {
+                return(
+                    <div onClick={() => deleteFromDB(item.id)}>
+                        <h1>{item.firstname}</h1>
+                        <h1>{item.id}</h1>
+                    </div>
+                );
+            })}
             <h1 className='titleStyle'>Bugs Manager</h1>
             <FadeIn transitionDuration='800' childTag='bodyStyle'>
                 <div className='bodyStyle'>
@@ -51,11 +124,7 @@ function Main() {
                     {firestoreData.length !== 0 &&
                         <div className='bugCardStyle'>
                             <FadeIn transitionDuration='800' childTag='bodyStyle'>
-                                {/* {firestoreData.map(item => { */}
-                                    {/* return ( */}
                                         <Bug firestoreData={firestoreData} />
-                                    {/* ); */}
-                                {/* })} */}
                             </FadeIn>
                         </div>
                     }
